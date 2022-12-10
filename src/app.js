f9dfc8cad1b383e102f6fc664f401fc7aad904b7
NataliiaGit <nataliukrkiev@gmail.com>
2022-12-10 23:27:48 +0100

    Update time

--- src/app.js
+++ src/app.js
@@ -15,3 +15,30 @@ let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=London&key=o8da94538210e4d3f40a1336t5f0ebb4&units=metric";
 
 axios.get(apiUrl).then(displayTemperature);
+
+function formatDate() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = "0${hours}";
+  }
+  let min = now.getMinutes();
+  if (min < 10) {
+    min = "0${min}";
+  }
+  let formattedDate = `${day}, ${hours}:${min}`;
+  return formattedDate;
+}
+
+let seetime = document.querySelector("#time");
+seetime.innerHTML = formatDate();
