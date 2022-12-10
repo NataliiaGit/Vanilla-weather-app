b50f9e6d126f02ff337fb3fb4458cad7ff97c839
NataliiaGit <nataliukrkiev@gmail.com>
2022-12-10 23:55:59 +0100

    Update time and date with app date

--- src/app.js
+++ src/app.js
@@ -1,23 +1,13 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = response.data.temperature.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-}
-
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=London&key=o8da94538210e4d3f40a1336t5f0ebb4&units=metric";
-
-axios.get(apiUrl).then(displayTemperature);
-
-function formatDate() {
-  let now = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -27,18 +17,26 @@ function formatDate() {
     "Friday",
     "Saturday",
   ];
-  let day = days[now.getDay()];
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = "0${hours}";
-  }
-  let min = now.getMinutes();
-  if (min < 10) {
-    min = "0${min}";
-  }
-  let formattedDate = `${day}, ${hours}:${min}`;
-  return formattedDate;
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-let seetime = document.querySelector("#time");
-seetime.innerHTML = formatDate();
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  timeElement.innerHTML = formatDate(response.data.time * 1000);
+}
+
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Madrid&key=o8da94538210e4d3f40a1336t5f0ebb4&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
